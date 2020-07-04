aec940e973b76eac0ffc927680b01a5e119436bb
Brandon Castellano <brandon248@gmail.com>
2020-07-04 15:16:45 -0400

    Begin work on flicker/flash suppression for ContentDetector.

--- scenedetect/detectors/content_detector.py
+++ scenedetect/detectors/content_detector.py
@@ -42,6 +42,21 @@ import cv2
 from scenedetect.scene_detector import SceneDetector
 
 
+def rgb_to_hsv(frame):
+    return cv2.split(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV))
+
+def calculate_frame_delta(curr_frame, last_frame):
+    delta_hsv = [0, 0, 0, 0]
+    for i in range(3):
+        num_pixels = curr_frame[i].shape[0] * curr_frame[i].shape[1]
+        curr_frame[i] = curr_frame[i].astype(numpy.int32)
+        last_frame[i] = last_frame[i].astype(numpy.int32)
+        delta_hsv[i] = numpy.sum(
+            numpy.abs(curr_frame[i] - last_frame[i])) / float(num_pixels)
+    delta_hsv[3] = sum(delta_hsv[0:3]) / 3.0
+    return delta_hsv
+
+
 class ContentDetector(SceneDetector):
     """Detects fast cuts using changes in colour and intensity between frames.
 
@@ -61,6 +76,10 @@ class ContentDetector(SceneDetector):
         self.last_hsv = None
         self._metric_keys = ['content_val', 'delta_hue', 'delta_sat', 'delta_lum']
         self.cli_name = 'detect-content'
+        self._new_scene_cut = None
+        self.flicker_frames = 0
+
+        #assert self.min_scene_len > self.flicker_frames
 
 
     def process_frame(self, frame_num, frame_img):
@@ -88,8 +107,40 @@ class ContentDetector(SceneDetector):
         if self.last_scene_cut is None:
             self.last_scene_cut = frame_num
 
+        # Handle flash suppression (i.e. validate a previously detected scene transition).
+        #
+        # TODO: Test rapid toggling.
+        #
+        # TODO: Add to statsfile (requires new column?, store N/A if no value,
+        #                         or delta from place where data starts otherwise)
+        #
+        if self._new_scene_cut is not None:
+            # Make sure the next N [flicker_frames] frames are also above the threshold.
+            if (frame_num - self._new_scene_cut[0]) > self.flicker_frames:
+                # We passed the threshold!  (if ff = 0 should have same behavior as current)
+                #                           (if ff = 1 should ignore a 1-frame flash)
+                #                           (if ff = 2 should ignore a 2-frame flash, etc)
+                cut_list.append(self._new_scene_cut[0])
+                self.last_scene_cut = self._new_scene_cut[0]
+                self._new_scene_cut = None
+
+            # This has to be cleaned up a lot.
+
+            # Check if delta between last_frame and frame_img still exceeds threshold.
+            # if yes, do nothing so the branch above eventually fires.
+            #if not, clear the cut, because it was a flash.
+            curr_hsv = rgb_to_hsv(frame_img)
+            last_hsv = rgb_to_hsv(self._new_scene_cut[0])
+            delta_h, delta_s, delta_v, delta_hsv_avg = calculate_frame_delta(
+                curr_hsv, last_hsv)
+            if delta_hsv_avg < self.threshold:
+                # Returned to the same scene within threshold; ignore.
+                self._new_scene_cut = None
+
+
+
         # We can only start detecting once we have a frame to compare with.
-        if self.last_frame is not None:
+        elif self.last_frame is not None:
             # Change in average of HSV (hsv), (h)ue only, (s)aturation only, (l)uminance only.
             # These are refered to in a statsfile as their respective self._metric_keys string.
             delta_hsv_avg, delta_h, delta_s, delta_v = 0.0, 0.0, 0.0, 0.0
@@ -100,21 +151,12 @@ class ContentDetector(SceneDetector):
                     frame_num, metric_keys)
 
             else:
-                num_pixels = frame_img.shape[0] * frame_img.shape[1]
-                curr_hsv = cv2.split(cv2.cvtColor(frame_img, cv2.COLOR_BGR2HSV))
+                curr_hsv = rgb_to_hsv(frame_img)
                 last_hsv = self.last_hsv
                 if not last_hsv:
-                    last_hsv = cv2.split(cv2.cvtColor(self.last_frame, cv2.COLOR_BGR2HSV))
-
-                delta_hsv = [0, 0, 0, 0]
-                for i in range(3):
-                    num_pixels = curr_hsv[i].shape[0] * curr_hsv[i].shape[1]
-                    curr_hsv[i] = curr_hsv[i].astype(numpy.int32)
-                    last_hsv[i] = last_hsv[i].astype(numpy.int32)
-                    delta_hsv[i] = numpy.sum(
-                        numpy.abs(curr_hsv[i] - last_hsv[i])) / float(num_pixels)
-                delta_hsv[3] = sum(delta_hsv[0:3]) / 3.0
-                delta_h, delta_s, delta_v, delta_hsv_avg = delta_hsv
+                    last_hsv = rgb_to_hsv(self.last_frame)
+                delta_h, delta_s, delta_v, delta_hsv_avg = calculate_frame_delta(
+                    curr_hsv, last_hsv)
 
                 if self.stats_manager is not None:
                     self.stats_manager.set_metrics(frame_num, {
@@ -129,11 +171,22 @@ class ContentDetector(SceneDetector):
             # the minimum scene length has been reached (otherwise it is ignored).
             if delta_hsv_avg >= self.threshold and (
                     (frame_num - self.last_scene_cut) >= self.min_scene_len):
-                cut_list.append(frame_num)
-                self.last_scene_cut = frame_num
+
+                # Move into 'validating scene transition' mode - wait until
+                # the next frame - make sure that one also has a delta
+                # above the threshold.
+                #
+                # Might need a state machine, heh.
+                #
+                if self.flicker_frames > 0:
+                    self._new_scene_cut = (frame_num, self.last_frame)
+                else:
+                    cut_list.append(frame_num)
+                    self.last_scene_cut = frame_num
 
             if self.last_frame is not None and self.last_frame is not _unused:
                 del self.last_frame
+                self.last_frame = None
 
         # If we have the next frame computed, don't copy the current frame
         # into last_frame since we won't use it on the next call anyways.
